370cc6250fadd7ecf4082bac3c4df2b8ec0c29c8
unknown <norbert2923@gmail.com>
2021-09-20 11:07:34 +0200

    Panda Calculator mini projekt könyvtár hozzáadva a repohoz

---
+++ Panda_Calculator/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panda Calculator</title>
+</head>
+<body>
+
+    <section style="background-color: gray; height: 200px; width: auto;">
+
+    </section>
+    
+</body>
+</html>
