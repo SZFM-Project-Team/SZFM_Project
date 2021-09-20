e83001d48967affa04088abb20f8d9f5e7934ca8
unknown <norbert2923@gmail.com>
2021-09-20 11:39:21 +0200

    Váz elkezdve a weboldalhoz

--- Panda_Calculator/index.php
+++ Panda_Calculator/index.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Panda Calculator</title>
 </head>
 <body>
+    
+    <section id="section-1" class="site-title site-header">
 
-    <section style="background-color: gray; height: 200px; width: auto;">
+    </section>
+    <section id="section-2" class="numbers-line ">
+
+    </section>
+    <section>
 
     </section>
     
