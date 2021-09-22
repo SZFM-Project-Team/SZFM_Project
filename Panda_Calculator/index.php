366443ab718bb46fd4c3f4ac426aa22fc1eef196
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 22:27:29 +0200

    index.php-hoz a váz elkészítve

--- Panda_Calculator/index.php
+++ Panda_Calculator/index.php
@@ -7,17 +7,81 @@
     <link rel="stylesheet" href="style.css">
     <title>Panda Calculator</title>
 </head>
+
 <body>
     
-    <section id="section-1" class="site-title site-header">
-
+    <section id="section-1" class="site-title site-header" style="height: 100px; background-color: red;">
+        
     </section>
-    <section id="section-2" class="numbers-line ">
 
+    <section id="section-2" class="numbers-line" style="height: 100px; background-color: gray;">
+        <div class="input-line">
+            <span class="input-line-inner">
+
+            </span>
+        </div>
     </section>
-    <section>
+
+    <section id="section-3" style="height: 600px; display: flex;">
+        <div class="number-panel" style="width: 27%; height: 600px; background-color: blue;">
+            <table>
+                <tr class="number-panel-first">
+                    <td><button type="button" class="button-7"></button></td>
+                    <td><button type="button" class="button-8"></button></td>
+                    <td><button type="button" class="button-9"></button></td>
+                </tr>
+                <tr class="number-panel-second">
+                    <td><button type="button" class="button-4"></button></td>
+                    <td><button type="button" class="button-5"></button></td>
+                    <td><button type="button" class="button-6"></button></td>
+                </tr>
+                <tr class="number-panel-third">
+                    <td><button type="button" class="button-1"></button></td>
+                    <td><button type="button" class="button-2"></button></td>
+                    <td><button type="button" class="button-3"></button></td>
+                </tr>
+                <tr class="number-panel-fourth">
+                    <td><button type="button" class="button-plus-minus"></button></td>
+                    <td><button type="button" class="button-0"></button></td>
+                    <td><button type="button" class="button-point"></button></td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="divider" style="width: 6%; height: 600px; background-color: red;">
+
+        </div>
+
+        <div class="operation-panel" style="width: 27%; height: 600px; background-color: purple;">
+            <table>
+                <tr class="operation-panel-first">
+                    <td><button type="button" class="button-x"></button></td>
+                    <td><button type="button" class="button-divide"></button></td>
+                    <td><button type="button" class="button-percentage"></button></td>
+                </tr>
+                <tr class="operation-panel-second">
+                    <td><button type="button" class="button-addition"></button></td>
+                    <td><button type="button" class="button-square"></button></td>
+                    <td><button type="button" class="button-clear"></button></td>
+                </tr>
+                <tr class="operation-panel-third">
+                    <td><button type="button" class="button-substract"></button></td>
+                    <td><button type="button" class="button-squareroot"></button></td>
+                    <td><button type="button" class="button-clearempty"></button></td>
+                </tr>
+                <tr class="operation-panel-fourth">
+                    <td><button type="button" class="button-equal"></button></td>
+                    <td><button type="button" class="button-backspace"></button></td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="panda-logo" style="width: 40%; height: 600px; background-color: yellow;">
+            <img src="" />
+        </div>
 
     </section>
     
 </body>
+
 </html>
