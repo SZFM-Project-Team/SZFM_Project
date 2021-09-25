0a3bfe14f6536f2b5124fdb87d349465462619ee
Szakács Ági <szakacs.agi21@gmail.com>
2021-09-25 23:23:37 +0200

    Button-style class hozzáadása a buttonokhoz az index.php-ba

--- Panda_Calculator/index.php
+++ Panda_Calculator/index.php
@@ -9,7 +9,7 @@
     <script type="text/javascript" src="./javascript/Addition.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="itthoni.css">
+    <link rel="stylesheet" href="style.css">
     <title>Panda Calculator</title>
 </head>
 
@@ -22,10 +22,6 @@
 
         function operationCheck(oper) 
         {
-            if (oper == 0)
-            {
-                break;
-            }
             switch oper:
                 case 1:
                     
@@ -40,7 +36,7 @@
     </script>
 
     <div class="container">
-        <img src="kek_csik.png" class="blue-line" alt="kek_csik" style="width:100%;">
+        <img src="kek_csik.png" class="blue-line" alt="kek_csik">
         <div id="label" class="text-block">
             <h1>PANDA CALCULATOR</h1>
         </div>
@@ -49,68 +45,69 @@
 
     <div class="input-line">
         <form name="cal">
-            <input type="text" name="display">     
+            <input type="text" id="display">     
         </form>
     </div>
    
     <section id="section-3" style="height: 600px; display: flex;">
-        <div class="number-panel" style="width: 27%; height: 600px; background-color: blue;">
-            <table>
-                <tr class="number-panel-first">
-                    <td><input value=" 7 " onClick="cal.display.value += '7'" type="button"></td>
-                    <td><input value=" 8 " onClick="cal.display.value += '8'" type="button"></td>
-                    <td><input value=" 9 " onClick="cal.display.value += '9'" type="button"></td>
+        <div class="container1">
+            <table class="middle">
+            <tr class="number-panel-first">
+                    <td><input value=" 7 " onClick="cal.display.value += '7'" type="button" class="button-style"></td>
+                    <td><input value=" 8 " onClick="cal.display.value += '8'" type="button" class="button-style"></td>
+                    <td><input value=" 9 " onClick="cal.display.value += '9'" type="button" class="button-style"></td>
                 </tr>
                 <tr class="number-panel-second">
-                    <td><input value=" 4 " onClick="cal.display.value += '4'" type="button"></td>
-                    <td><input value=" 5 " onClick="cal.display.value += '5'" type="button"></td>
-                    <td><input value=" 6 " onClick="cal.display.value += '6'" type="button"></td>
+                    <td><input value=" 4 " onClick="cal.display.value += '4'" type="button" class="button-style"></td>
+                    <td><input value=" 5 " onClick="cal.display.value += '5'" type="button" class="button-style"></td>
+                    <td><input value=" 6 " onClick="cal.display.value += '6'" type="button" class="button-style"></td>
                 </tr>
                 <tr class="number-panel-third">
-                    <td><input value=" 1 " onClick="cal.display.value += '1'" type="button"></td>
-                    <td><input value=" 2 " onClick="cal.display.value += '2'" type="button"></td>
-                    <td><input value=" 3 " onClick="cal.display.value += '3'" type="button"></td>
+                    <td><input value=" 1 " onClick="cal.display.value += '1'" type="button" class="button-style"></td>
+                    <td><input value=" 2 " onClick="cal.display.value += '2'" type="button" class="button-style"></td>
+                    <td><input value=" 3 " onClick="cal.display.value += '3'" type="button" class="button-style"></td>
                 </tr>
                 <tr class="number-panel-fourth">
-                    <td><input value="+/-" onClick="cal.display.value = " type="button"></td>
-                    <td><input value=" 0 " onClick="cal.display.value += '0'" type="button"></td>
-                    <td><input value=" . " onClick="cal.display.value += '.'" type="button"></td>
+                    <td><input value="+/-" onClick="cal.display.value = " type="button" class="button-style"></td>
+                    <td><input value=" 0 " onClick="cal.display.value += '0'" type="button" class="button-style"></td>
+                    <td><input value=" . " onClick="cal.display.value += '.'" type="button" class="button-style"></td>
                 </tr>
             </table>
         </div>
 
-        <div class="divider" style="width: 6%; height: 600px; background-color: white;">
-        <div id="grey-line"></div>
+        <div class="container2" >
+        <div class="grey-line"></div>
         </div>
 
-        <div class="operation-panel" style="width: 27%; height: 600px; background-color: purple;">
-            <table>
-                <tr class="operation-panel-first">
-                    <td><input type="button" value=" x " onClick="x = cal.display.value; cal.display.value = ''; operationCheck();"></td>
-                    <td><input type="button" value=" / " onClick=""></td>
-                    <td><input type="button" value=" % " onClick=""></td>
+        <div class="container3" >
+            <table class="center">
+            <tr class="operation-panel-first">
+                    <td><input type="button" value=" x " onClick="x = cal.display.value; cal.display.value = '';"class="button-style"></td>
+                    <td><input type="button" value=" / " onClick="" class="button-style"></td>
+                    <td><input type="button" value=" % " onClick="" class="button-style"></td>
                 </tr>
                 <tr class="operation-panel-second">
-                    <td><input type="button" value=" + " onClick=""></td>
-                    <td><input type="button" value="x^2" onClick="cal.display.value = square(parseInt(cal.display.value)).toString();"></td>
-                    <td><input type="button" value=" C " onClick=""></td>
+                    <td><input type="button" value=" + " onClick="" class="button-style"></td>
+                    <td><input type="button" value="x^2" onClick="cal.display.value = square(parseInt(cal.display.value)).toString();" class="button-style"></td>
+                    <td><input type="button" value=" C " onClick="" class="button-style"></td>
                 </tr>
                 <tr class="operation-panel-third">
-                    <td><input type="button" value=" - " onClick=""></td>
-                    <td><input type="button" value=" √ " onClick="cal.display.value = sqrt(parseInt(cal.display.value)).toString();"></td>
-                    <td><input type="button" value=" CE" onClick=""></td>
+                    <td><input type="button" value=" - " onClick="" class="button-style"></td>
+                    <td><input type="button" value=" √ " onClick="cal.display.value = sqrt(parseInt(cal.display.value)).toString();" class="button-style"></td>
+                    <td><input type="button" value=" CE" onClick="" class="button-style"></td>
                 </tr>
                 <tr class="operation-panel-fourth">
-                    <td><input type="button" value=" = " onClick=""></td>
-                    <td><input type="button" value="<x|" onClick=""></td>
+                    <td><input type="button" value=" = " onClick="" class="button-style"></td>
+                    <td><input type="button" value="<x|" onClick="" class="button-style"></td>
                 </tr>
             </table>
         </div>
 
-        <div class="panda-logo" style="width: 40%; height: 600px; background-color: white;">
-            <a href="javascript:playSound('panda');"><img src="panda.png" alt="panda" /></a> 
+        <div class="panda-logo">
+        <a href="javascript:playSound('panda');"><img src="panda.png" alt="panda" class="panda-image"/></a> 
             <audio id="panda"  src="./song.mp3" preload="auto"></audio>
         </div>
+       
 
     </section>
     
