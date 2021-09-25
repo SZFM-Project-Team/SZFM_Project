a26cdcb52e474f15f5d3b69a271085b8d5461f33
BuildTools <unconfigured@null.spigotmc.org>
2021-09-25 23:46:17 +0200

    Equalation.js fájl hozzáadva

---
+++ Panda_Calculator/javascript/equalation.js
@@ -0,0 +1,28 @@
+// Ez a függvény az összeadás műveletet végzi
+function Equalation(oper,x,y)
+{
+	switch (oper)
+    {
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+            
+        case 5:
+            
+        case 6:
+            
+        case 7:
+
+        case 8:
+
+        case 9:
+
+        case 10:
+
+        case 11:
+    }
+}
