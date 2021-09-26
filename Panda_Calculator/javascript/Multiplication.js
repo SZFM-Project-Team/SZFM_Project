a03f32eaf7dd9f7fae4e6540ef7fa55ed4c64b0b
NaghiPatrick <naghipatrick16@gmail.com>
2021-09-26 16:13:20 -0700

    reklamkep designolva, addition es multiplication fuggvenyek kommentalva

--- Panda_Calculator/javascript/Multiplication.js
+++ Panda_Calculator/javascript/Multiplication.js
@@ -1,9 +1,14 @@
 // Ez a függvény az szorzás műveletet végzi
 function Multiplication(x,y)
 {
+//A Multiplication fÜggvény két paramétert kap:
+//Ezek x és y , ezek segétkeznek majd a művelet elvégzésében.
+//Először meg kell bizonyosodnunk róla, hogy mind két paraméter számot kapott-e értékként.
+//Erre a következő if elágazást használjuk:
 	if(typeof(x) == "number"&&
 	   typeof(y) == "number")
 		{
+			//Ha mind a két paraméter szám akkor érdemes megvizsgálnunk, hogy bármelyik érték 0 e.
 			//Ha bármelyik érték 0 lenne :
 			//Az eredmény mindenféle képpen 0;
 			//így kihagyhatjuk a számolás részét, időt spórolva.
@@ -11,14 +16,18 @@ function Multiplication(x,y)
 			{
 				return 0;
 			}
+			//Ha egyik érték sem 0, akkor nyugodtan elvégezhetjük a művelet.
 			//szorzás
 			let result;
+			//itt a result változó ez fogja tárolna a művelet végeredményét.
 			result = x * y;
 			return result;
 		}
 		else
 		{
+			//Ha valamelyik(mindkét) paraméter értéke nem "Szám" , akkor hibás a megadott adat.
 			//Hibás adat lett megadva
+			//Ebben az esetben egy hibaüzenet iratódik ki a felhasználó számára.
 			alert("Az input nem 'szám' típusú!!");
 		}
 }
